2d094be21f82542c5cfca56b2f2ef674b65cc545
user <user@DESKTOP-DKMASEA>
2023-09-12 11:08:29 +0900

    Mapper

--- hompage/src/main/java/com/example/demo/notice/dao/NoticeDao.java
+++ hompage/src/main/java/com/example/demo/notice/dao/NoticeDao.java
@@ -1,11 +1,15 @@
 package com.example.demo.notice.dao;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface NoticeDao {
+	@Insert()
+	public Long save(String aaaa);
 	
-	public Long aa(String aaaa);
-	
+	@Select()
+	public Long aa(String aa);
 
 }
