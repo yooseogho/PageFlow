92ef685ca699021f1d8fcb9b237ade71038fa58b
user <user@DESKTOP-K03K7F2>
2023-09-12 17:11:12 +0900

    MemberDao

--- hompage/src/main/java/com/example/demo/member/dao/MemberDao.java
+++ hompage/src/main/java/com/example/demo/member/dao/MemberDao.java
@@ -6,6 +6,53 @@ import com.example.demo.member.entity.*;
 
 @Mapper
 public interface MemberDao {
-	@Select("select * from member" )
-	public Member findById(String username);
+	// 회원가입
+	@Insert("insert into member values(#{memberId}, #{gradeCode}, #{memberName}, #{memberEmail}, #{password}, #{memberAddress}, #{memberTel}, #{memberProfile}, #{memberPoint}, #{joinday}, #{birthday})")	
+	public Integer save(Member member);
+
+	// 아이디 중복
+	@Select("select * from member where member_id=#{memberId} and rownum=1")
+	public Member findById(String memberId);
+	
+	// 전화번호 중복
+	@Select("select * from member where member_tel=#{memberTel} and rownum=1")
+	public Member findByTel(String memberTel);
+	
+	// 내정보 보기
+	@Select("select * from member where password=#{password} and rownum=1")
+	public Member findAll(String password);
+	
+	// 비밀번호 변경
+	@Update("update member set password=#{password} where password=#{password}")
+	public Integer changPassword(String password);
+	
+	// 이름 변경
+	@Update("update member set member_name=#{memberName} where password=#{password}")
+	public Integer changMemberName(String memberName, String password);
+	
+	// 이메일 변경
+	@Update("update member set member_email=#{memberEmail} where password=#{password}")
+	public Integer changMemberEmail(String memberEmail, String password);
+	
+	// 전화번호 변경
+	@Update("update member set member_tel=#{memberTel} where password=#{password}")
+	public Integer changMemberTel(String memberTel, String password);
+	
+	// 프로필사진 변경
+	@Update("update member set member_profile=#{memberProfile} where password=#{password}")
+	public Integer changProfile(String memberProfile, String password);
+	
+	// 회원 탈퇴
+	@Delete("delete from member where password=#{password}")
+	public Integer deleteById(String password);
+	
+	// 아이디 찾기 - 이메일
+	@Select("select memberId from member where member_email=#{memberEmail}")
+	public String findIdByEmail(String memberEmail);
+	
+	// 아이디 찾기 - 전화번호
+	@Select("select memberId from member where member_tel=#{memberTel}")
+	public String findIdByTel(String memberTel);
 }
+
+
