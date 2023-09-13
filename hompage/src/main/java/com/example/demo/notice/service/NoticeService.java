677286120baa97907c4c5ed452b1040e84b86cd1
user <user@DESKTOP-TMDOSVL>
2023-09-13 09:05:40 +0900

    NoticeService 수정

---
+++ hompage/src/main/java/com/example/demo/notice/service/NoticeService.java
@@ -0,0 +1,52 @@
+package com.example.demo.notice.service;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+
+import com.example.demo.notice.dao.*;
+import com.example.demo.notice.entity.*;
+
+@Service
+public class NoticeService {
+  @Autowired
+  private NoticeDao noticeDao;
+  
+  public Boolean insertNotice(String noticeTitle, String noticeContent) {
+    if(noticeTitle.isEmpty()||noticeTitle.isBlank()||noticeContent.isEmpty()||noticeContent.isBlank()) {
+      return false;
+    }
+    
+    Notice notice = new Notice();
+    notice.setNoticeTitle(noticeTitle);
+    notice.setNoticeContent(noticeContent);
+    
+    return noticeDao.save(notice)==1;
+  }
+  
+  public Boolean deleteNotice(Long nno) {
+    if(nno==null) {
+      return false;
+    }
+    Integer deleteRows = noticeDao.deleteByNno(nno);
+    return deleteRows==1;
+  }
+  
+  public Boolean updateNotice(Long nno, String noticeTitle, String noticeContent) {
+    if(nno==null) {
+      return false;
+    }
+    return noticeDao.updateNotice(nno, noticeTitle, noticeContent)==1;
+  }
+  
+  public Notice checkNotice(Long nno) {
+    if(nno==null) {
+      return null;
+    }
+    return noticeDao.findRead(nno);
+  }
+  
+  public Boolean count() {
+    return noticeDao.count()==1;
+  }
+  
+}
