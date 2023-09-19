d458fe307b0673d8b17a20171a9a078afb77840f
user <user@DESKTOP-K03K7F2>
2023-09-19 09:41:01 +0900

    OrdersDao, authorDao

---
+++ hompage/src/main/java/com/example/demo/dao/orders/OrdersDao.java
@@ -0,0 +1,29 @@
+package com.example.demo.dao.orders;
+
+import java.util.*;
+
+import org.apache.ibatis.annotations.*;
+
+import com.example.demo.dto.orders.*;
+import com.example.demo.entity.inquiry.*;
+import com.example.demo.entity.orderDetails.*;
+import com.example.demo.entity.orders.*;
+
+@Mapper
+public interface OrdersDao {
+	// 주문내역 저장
+	@Insert("INSERT INTO orders (ono, member_id, dno, order_date, order_request, order_price, order_status, payment, point_earn) "
+			+ "VALUES (#{ono}, #{memberId}, #{dno}, #{orderDate}, #{orderRequest}, #{orderPrice}, #{orderStatus}, #{payment}, #{pointEarn})")
+	public Integer save(Orders orders);
+
+	// 주문내역 전체 보기
+	public List<Orders> findAllOrders(String memberId);
+	
+	// 주문내역 상세 보기
+	public Orders findByOrderDetails(Long ono);
+
+	// 주문 정보 삭제
+	@Delete("DELETE FROM orders WHERE ono=#{ono}")
+	public Integer deleteByOrders(Long ono);
+
+}
