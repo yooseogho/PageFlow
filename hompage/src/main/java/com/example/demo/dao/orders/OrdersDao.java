9f58509440bc6a476796f9a559b67d0bd2344556
user <user@DESKTOP-K03K7F2>
2023-09-21 13:45:09 +0900

    OrdersDao, OrdersDto, Orders-mapper

--- hompage/src/main/java/com/example/demo/dao/orders/OrdersDao.java
+++ hompage/src/main/java/com/example/demo/dao/orders/OrdersDao.java
@@ -13,15 +13,15 @@ public interface OrdersDao {
 	@Insert("INSERT INTO orders (ono, member_id, dno, order_date, order_request, order_price, order_status, payment, point_earn) "
 			+ "VALUES (#{ono}, #{memberId}, #{dno}, #{orderDate}, #{orderRequest}, #{orderPrice}, #{orderStatus}, #{payment}, #{pointEarn})")
 	public Integer save(Orders orders);
-	
-	// 주문내역 전체 보기
-	public List<OrdersDto> findAllOrders();
-	
+
+	// 주문내역 전체 보기(주문/배송 목록)
+	public List<OrdersDto> findAllOrders(@Param("memberId") String memberId);
+
 	// 주문내역 상세 보기
-	OrdersDto findByOrderDetails(Long ono);
+	// 주문 상세 정보 조회
+	OrdersDto findByOrderDetails(@Param("memberId") String memberId, @Param("ono") Long ono);
 
-	// 주문 정보 삭제
+	// 주문내역 삭제
 	@Delete("DELETE FROM orders WHERE ono=#{ono}")
-	public Integer deleteByOrders(Long ono);
-
+	public Integer deleteByOrders(String memberId);
 }
--- hompage/src/main/java/com/example/demo/dto/orders/OrdersDto.java
+++ hompage/src/main/java/com/example/demo/dto/orders/OrdersDto.java
@@ -6,7 +6,7 @@ import lombok.*;
 @Data
 @AllArgsConstructor
 public class OrdersDto {
-	private int ono;
+	private Long ono;
 	private String bookTitle;
 	private String imageName;
 	private Long orderTotal;
@@ -14,68 +14,5 @@ public class OrdersDto {
 	private Long totalOrderCount;
 	private String zipCode;
 	private String receiverName;
-
-	public int getOno() {
-		return ono;
-	}
-
-	public String getBookTitle() {
-		return bookTitle;
-	}
-
-	public String getImageName() {
-		return imageName;
-	}
-
-	public Long getOrderTotal() {
-		return orderTotal;
-	}
-
-	public Long getTotalBookPrice() {
-		return totalBookPrice;
-	}
-
-	public Long getTotalOrderCount() {
-		return totalOrderCount;
-	}
-
-	public String getZipCode() {
-		return zipCode;
-	}
-
-	public String getReceiverName() {
-		return receiverName;
-	}
-
-	public void setOno(int ono) {
-		this.ono = ono;
-	}
-
-	public void setBookTitle(String bookTitle) {
-		this.bookTitle = bookTitle;
-	}
-
-	public void setImageName(String imageName) {
-		this.imageName = imageName;
-	}
-
-	public void setOrderTotal(Long orderTotal) {
-		this.orderTotal = orderTotal;
-	}
-
-	public void setTotalBookPrice(Long totalBookPrice) {
-		this.totalBookPrice = totalBookPrice;
-	}
-
-	public void setTotalOrderCount(Long totalOrderCount) {
-		this.totalOrderCount = totalOrderCount;
-	}
-
-	public void setZipCode(String zipCode) {
-		this.zipCode = zipCode;
-	}
-
-	public void setReceiverName(String receiverName) {
-		this.receiverName = receiverName;
-	}
+	private String memberId;
 }
