6b9247cd7a5ca28c3534e52994ac8c67c13033fb
user <user@DESKTOP-DKMASEA>
2023-09-22 16:55:01 +0900

    orderDetailsDao 유석호

--- hompage/src/main/java/com/example/demo/dao/orderDetails/OrderDetailsDao.java
+++ hompage/src/main/java/com/example/demo/dao/orderDetails/OrderDetailsDao.java
@@ -6,23 +6,50 @@ import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
 import org.apache.ibatis.annotations.Update;
 
 import com.example.demo.entity.orderDetails.OrderDetails;
 @Mapper
 public interface OrderDetailsDao {
 
-    // 주문 상세 생성
-    @Insert("INSERT INTO order_details (bno, ono, order_count, order_total, book_title) VALUES (#{bno}, #{ono}, #{orderCount}, #{orderTotal}, #{bookTitle})")
+     // 주문 상세 생성
+	 @SelectKey(statement = "select order_details__seq.nextval from dual",before = true, resultType = long.class,keyProperty="ono")
+     @Insert("INSERT INTO order_details (bno, ono, order_count, order_total, book_title) VALUES (#{bno}, #{ono}, #{orderCount}, #{orderTotal}, #{bookTitle})")
      public Integer insertOrderDetails(OrderDetails orderDetails);
 
     // 특정 주문의 모든 주문 상세 조회
     @Select("SELECT * FROM order_details WHERE ono = #{ono}")
     public List<OrderDetails> selectOrderDetailsByOno(Long ono);
 
-    // 특정 주문 상세 조회
-    @Select("SELECT * FROM order_details WHERE bno = #{bno} AND ono = #{ono}")
-    public OrderDetails selectOrderDetailByBnoAndOno(Long bno, Long ono);
+    // 주문상세 전체조회
+    @Select("SELECT * FROM order_details")
+    public List<OrderDetails> findAll();
+    
+    
+	// 회원의 주문 내역 조회
+	public List<OrderDetails> findDetailsByMemberId(String memberId);
+	
+	
+	// 취소 신청 
+	@Update("UPDATE order_details SET order_status = '주문취소' WHERE bno = #{bno} AND member_id = #{memberId}")
+	public Integer changeStatus(String bno, String memberId);
 
+	
+	
+	
+	
+	
+	
+	
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
---
+++ hompage/src/main/java/com/example/demo/dto/orderDetails/OrderDetailsDto.java
@@ -0,0 +1,22 @@
+package com.example.demo.dto.orderDetails;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class OrderDetailsDto {
+
+	// 장바구니에서 상품 주문하고 결제하는 확인 페이지 에서 쓸 DTO
+	@Data
+	@AllArgsConstructor
+	public static class OrderDetailsWithBookInfo {
+		private String imageName;
+		private String bookTitle;
+		private Integer bookPrice;
+		private Integer cartCount;
+		private Integer totalPrice;
+
+	}
+}
