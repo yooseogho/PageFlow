fea0fa5c24ef317b1d1d454c0d86366518a538b6
user <user@DESKTOP-K03K7F2>
2023-09-27 17:00:10 +0900

    OrderDao

--- hompage/src/main/java/com/example/demo/dao/orders/OrdersDao.java
+++ hompage/src/main/java/com/example/demo/dao/orders/OrdersDao.java
@@ -4,30 +4,33 @@ import java.util.*;
 
 import org.apache.ibatis.annotations.*;
 
-import com.example.demo.dto.orders.*;
 import com.example.demo.entity.orders.*;
 
 @Mapper
-public interface OrdersDao { 
-  
-//	전체 조회
-  @Select("select * from orders")
-  public List<Orders> getAllOrders();
-  
-	// 주문내역 저장
-	@Insert("INSERT INTO orders (ono, member_id, dno, order_date, order_request, order_price, order_status, payment, point_earn) "
-			+ "VALUES (#{ono}, #{memberId}, #{dno}, #{orderDate}, #{orderRequest}, #{orderPrice}, #{orderStatus}, #{payment}, #{pointEarn})")
+public interface OrdersDao {
+	// 1. 주문내역 저장
+	@Insert("INSERT INTO orders (ono, member_id, dno, order_date, order_request, order_price, payment, point_earn) "
+			+ "VALUES (#{ono}, #{memberId}, #{dno}, #{orderDate}, #{orderRequest}, #{orderPrice}, #{payment}, #{pointEarn})")
 	public Integer save(Orders orders);
 
-	// 주문내역 전체 보기(주문/배송 목록)
-	public List<OrdersDto> findAllOrders(@Param("memberId") String memberId);
+	// 2. 전체 조회
+	@Select("select * from orders")
+	public List<Orders> getAllOrders();
 
-	// 주문내역 상세 보기
-	// 주문 상세 정보 조회
-	public Orders findByOrderDetails(@Param("memberId") String memberId, @Param("ono") Long ono);
+	// 3. 장바구니 - 결제예정금액
+	public Orders getCartInfoByTotalPay(String memberId);
 
-	// 주문내역 삭제
+	// 4. 주문/결제 - 결제예정금액
+	public Orders getOrderInfoByTotalPay(String memberId);
+
+	// 5. 주문내역 삭제
 	@Delete("DELETE FROM orders WHERE ono=#{ono} and member_id=#{memberId}")
 	public Integer deleteByOrders(Long ono, String memberId);
+
+	// 6. 사용자 주문/배송 목록 리스트
+	@Select("select o.ono, bi.image_name, b.book_title, od.order_count, (sum(od.order_count * b.book_price), od.order_status)"
+			+ " from orders o inner join order_details od on o.ono=od.ono inner join member m on m.member_id=o.member_id inner join book b on b.bno=od.bno inner join book_image bi on bi.bno=b.bno group by  o.ono, bi.image_name, b.book_title, od.order_count, od.order_count, b.book_price, od.order_status")
+	List<Orders> findAllOrders(String memberId);
 	
+
 }
