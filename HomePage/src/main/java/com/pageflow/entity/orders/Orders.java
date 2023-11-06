1a202b037353536b248a58ee95c6f7eafb197f52
유석호 <tjrgh6362@naver.com>
2023-11-06 14:28:38 +0900

    sql.txt/Orders 엔티티 수정/유석호

--- HomePage/src/main/java/com/pageflow/entity/orders/Orders.java
+++ HomePage/src/main/java/com/pageflow/entity/orders/Orders.java
@@ -1,21 +1,21 @@
-package com.pageflow.entity.orders;
-
-import java.time.*;
-
-import org.springframework.format.annotation.*;
-
-import lombok.*;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Orders {
-	private Long ono;
-	private String memberId;
-	private Long dno;
-	private LocalDate orderDate;
-	private Long orderPrice;
-	private String payment;
-	private Long pointEarn;
-	private String ordersBuyer;
-}
+	package com.pageflow.entity.orders;
+	
+	import java.time.*;
+	
+	import org.springframework.format.annotation.*;
+	
+	import lombok.*;
+	
+	@Data
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public class Orders {
+		private Long ono;
+		private String memberId;
+		private Long dno;
+		private LocalDate orderDate;
+		private Long orderPrice;
+		private String payment;
+		private Long pointEarn;
+		private String ordersBuyer;
+	}
